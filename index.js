29b8d2f1183eefc85d04d1b85b546841ca104088
Nikk7 <n.sliyusar@gmail.com>
2023-11-11 18:55:15 +0200

    edit list

--- index.js
+++ index.js
@@ -7,136 +7,136 @@ let activePrimary = false;
 
 const mapHtml = '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5873.503522773722!2d-122.40700885535698!3d37.75875723485357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sua!4v1699547569699!5m2!1sen!2sua"  height="450" style="border:0; width:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
 
-const listHtml = `<div class="row align-items-center row-cols-lg-3 g-lg-4 row-cols-md-2 row-cols-sm-1 g-md-4 g-sm-3 g-2">
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-       <div class="store-img">
-           <img src="./assets/images/stores/store1.png" alt="store1">
-       </div>
-       <div class="store-name">
-           <h5>Store name</h5>
-       </div>
-       <div class="store-zip">
-           <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-       </div>
-       <div class="btn">
-           <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-       </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-        <div class="store-img">
-            <img src="./assets/images/stores/store2.png" alt="store2">
-        </div>
-        <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-        <div class="store-img">
-            <img src="./assets/images/stores/store3.png" alt="store3">
-        </div>
-        <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-        <div class="store-img">
-            <img src="./assets/images/stores/store4.png" alt="store4">
-        </div>
-        <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-       <div class="store-img">
-           <img src="./assets/images/stores/store5.png" alt="store5">
-       </div>
-       <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-        <div class="store-img">
-            <img src="./assets/images/stores/store6.png" alt="store6">
-        </div>
-        <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-        <div class="store-img">
-            <img src="./assets/images/stores/store7.png" alt="store7">
-        </div>
-        <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-<div class="col ">
-    <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
-        <div class="store-img">
-            <img src="./assets/images/stores/store8.png" alt="store8">
-        </div>
-        <div class="store-name">
-            <h5>Store name</h5>
-        </div>
-        <div class="store-zip">
-            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
-        </div>
-        <div class="btn">
-            <button type="button" class=" btn btn-primary px-4 py-2">View</button>
-        </div>
-    </div>
-</div>
-</div>`
+const listHtml = `<div class="row align-items-center row-cols-lg-3 g-lg-4 row-cols-md-2 row-cols-sm-2 g-md-4 g-sm-3 g-4">
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                        <div class="store-img">
+                            <img src="./assets/images/stores/store1.png" alt="store1">
+                        </div>
+                        <div class="store-name">
+                            <h5>Store name</h5>
+                        </div>
+                        <div class="store-zip">
+                            <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                        </div>
+                        <div class="btn store-list-btn">
+                            <button type="button" class=" btn  px-4 py-2">View</button>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                            <div class="store-img">
+                                <img src="./assets/images/stores/store2.png" alt="store2">
+                            </div>
+                            <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                            <div class="store-img">
+                                <img src="./assets/images/stores/store3.png" alt="store3">
+                            </div>
+                            <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                            <div class="store-img">
+                                <img src="./assets/images/stores/store4.png" alt="store4">
+                            </div>
+                            <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                        <div class="store-img">
+                            <img src="./assets/images/stores/store5.png" alt="store5">
+                        </div>
+                        <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                            <div class="store-img">
+                                <img src="./assets/images/stores/store6.png" alt="store6">
+                            </div>
+                            <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                            <div class="store-img">
+                                <img src="./assets/images/stores/store7.png" alt="store7">
+                            </div>
+                            <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ">
+                        <div class="item-card border rounded d-flex justify-content-lg-start align-items-center flex-column overflow-hidden">
+                            <div class="store-img">
+                                <img src="./assets/images/stores/store8.png" alt="store8">
+                            </div>
+                            <div class="store-name">
+                                <h5>Store name</h5>
+                            </div>
+                            <div class="store-zip">
+                                <p>8961 North Blackburn Ave.Milton, MA 02186</p>
+                            </div>
+                            <div class="btn store-list-btn">
+                                <button type="button" class=" btn  px-4 py-2">View</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>`
 
 let togleMap = true;
 
@@ -210,11 +210,20 @@ $('.list-map-btn').on('click', function(){
     if (!togleMap) {
         $('.list-map').empty();
         $('.list-map').append(mapHtml);
+        $('#viewMap').addClass('hidden');
+        $('#viewList').removeClass('hidden');
+        $('.view-map-img').addClass('hidden');
+        $('.view-list-img').removeClass('hidden');
         togleMap = true;
 
     } else {
         $('.list-map').empty();
         $('.list-map').append(listHtml);
+        $('#viewMap').removeClass('hidden');
+        $('#viewList').addClass('hidden');
+        $('.view-map-img').removeClass('hidden');
+        $('.view-list-img').addClass('hidden');
+
         togleMap = false;
 
     }
